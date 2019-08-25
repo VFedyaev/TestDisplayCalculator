a6140e860924036ae1dad28530b10b64812a1e82
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-08-25 19:19:46 +0600

    add Division step by step, without display in column

--- DisplayCalculator/DisplayCalculator/Calculator.cs
+++ DisplayCalculator/DisplayCalculator/Calculator.cs
@@ -34,10 +34,20 @@ namespace DisplayCalculator
                 Console.WriteLine("Result is: " + result);
                 Console.WriteLine("---------------------------------------");
 
-                var table = new ConsoleTable(values.Item1.ToString());
-                table.AddRow(values.Item2)
-                     .AddRow(result);
-                table.Write(Format.AlternativeAddition);
+                if (values.Item1.ToString().Length >= values.Item2.ToString().Length)
+                {
+                    var table = new ConsoleTable(values.Item1.ToString());
+                    table.AddRow(values.Item2)
+                         .AddRow(result);
+                    table.Write(Format.AlternativeAddition);
+                }
+                else
+                {
+                    var table = new ConsoleTable(values.Item2.ToString());
+                    table.AddRow(values.Item1)
+                         .AddRow(result);
+                    table.Write(Format.AlternativeAddition);
+                }
             }
             catch (FormatException)
             {
@@ -63,6 +73,7 @@ namespace DisplayCalculator
                 table.AddRow(values.Item2)
                      .AddRow(result);
                 table.Write(Format.AlternativeSubstraction);
+
             }
             catch (FormatException)
             {
@@ -84,10 +95,20 @@ namespace DisplayCalculator
                 Console.WriteLine("Result is: " + result);
                 Console.WriteLine("---------------------------------------");
 
-                var table = new ConsoleTable(values.Item1.ToString());
-                table.AddRow(values.Item2)
-                     .AddRow(result);
-                table.Write(Format.AlternativeMultiplication);
+                if (values.Item1.ToString().Length >= values.Item2.ToString().Length)
+                {
+                    var table = new ConsoleTable(values.Item1.ToString());
+                    table.AddRow(values.Item2)
+                         .AddRow(result);
+                    table.Write(Format.AlternativeMultiplication);
+                }
+                else
+                {
+                    var table = new ConsoleTable(values.Item2.ToString());
+                    table.AddRow(values.Item1)
+                         .AddRow(result);
+                    table.Write(Format.AlternativeMultiplication);
+                }
             }
             catch (FormatException)
             {
@@ -104,6 +125,7 @@ namespace DisplayCalculator
         {
             try
             {
+                string continueId;
                 var values = InputValues();
                 double result;
 
@@ -119,6 +141,14 @@ namespace DisplayCalculator
                     table.AddRow(values.Item2)
                          .AddRow(result);
                     table.Write(Format.AlternativeDivision);
+
+                    Console.WriteLine("Show the Division step by step? Y/N");
+                    continueId = Console.ReadLine();
+                    if (continueId.ToUpper() == "Y")
+                    {
+                        Console.WriteLine();
+                        DivisionByStep(values);
+                    }                      
                 }
             }
             catch (FormatException)
@@ -128,6 +158,75 @@ namespace DisplayCalculator
             catch (Exception ex)
             {
                 Console.WriteLine("You try to do something wrong! " + ex);
+            }
+        }
+
+        public static void DivisionByStep(Tuple<double,double> userValues)
+        {
+            //P.S. Oh, I'm really sorry, for this code. Your poor eyes, yes i know what do u feel now. One more time sorry. But at this moment i can't come up with nothing better. Because i do it by my self and i have't a person to ask him mb better way. 
+            try
+            {
+                int compFirstValue = Convert.ToInt32(userValues.Item1 * 100), compSecondValue = Convert.ToInt32(userValues.Item2 * 100);
+                string answer = "";
+
+                Console.Write(userValues.Item1 + " / " + userValues.Item2 + " = " + compFirstValue + " / " + compSecondValue);
+                Console.WriteLine();
+
+                if (Int32.TryParse(userValues.Item1.ToString(), out int number1) && Int32.TryParse(userValues.Item2.ToString(), out int number2))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Answer is: = " + (Convert.ToDouble(Convert.ToDouble(number1) / Convert.ToDouble(number2))));
+                    Console.ReadKey();
+                }
+                else
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        int i = 1;
+                        int valueOfCurrentIteration = 0, valueOfPrevIteration = 0;
+
+                        do
+                        {
+                            valueOfCurrentIteration = Convert.ToInt32(compSecondValue * i);
+
+                            if (valueOfCurrentIteration >= compFirstValue)
+                                break;
+                            else
+                            {
+                                valueOfPrevIteration = valueOfCurrentIteration;
+                                i++;
+                            }
+                        } while (compFirstValue >= valueOfCurrentIteration);
+
+                        Console.WriteLine();
+                        Console.WriteLine(compSecondValue + " * " + --i + " = " + valueOfPrevIteration);
+                        Console.WriteLine(compFirstValue + " - " + valueOfPrevIteration + " = " + (compFirstValue - valueOfPrevIteration));
+
+                        compFirstValue = compFirstValue - valueOfPrevIteration;
+                        answer += i;
+
+                        while (compFirstValue < compSecondValue)
+                        {
+                            compFirstValue *= 10;
+                            answer += ",";
+                        };
+                        j++;
+                    }
+
+                    Console.WriteLine();
+
+                    answer = answer.Replace(",,", ",0");
+                    string stringBeforeChar = answer.Substring(0, answer.IndexOf(",") + 1);
+                    string stringAfterChar = answer.Substring(answer.IndexOf(",") + 1).Replace(",", "");
+                    answer = stringBeforeChar + stringAfterChar;
+
+                    Console.WriteLine("Answer is: = " + answer);
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
             }
         }
     }
